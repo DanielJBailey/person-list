83d4144358da0743aef91361c12c8791fddb3e1d
Daniel Bailey <daniel.justin.bailey@gmail.com>
2018-11-14 08:42:29 -0700

    finish assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  root "people#index"
+  get '/', to: 'people#index'
+  get '/home', to: 'people#index'
+  resources :people
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
